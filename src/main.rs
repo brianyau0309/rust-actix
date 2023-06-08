b458534633dbbcdcdd6a5cd9e6f0d3f742cfd96a
Brian Yau <brianyauu@gmail.com>
2023-06-08 22:50:58 +0800

    update: health check & connect database

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::net::TcpListener;
+use actix_demo::configuration::get_configuration;
+use actix_demo::startup::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
+}
