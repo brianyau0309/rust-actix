c7847553bae893f5bae904be601a0b932b7abbc5
Brian Yau <brianyauu@gmail.com>
2023-07-23 19:05:24 +0800

    validate request, add sending email, and refactor tests

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-use actix_demo::startup::run;
-use actix_demo::telemetry::init_subscriber;
-use actix_demo::{configuration::get_configuration, telemetry::get_subscriber};
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
+use actix_demo::configuration::get_configuration;
+use actix_demo::startup::Application;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
-    init_subscriber(subscriber);
-
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
